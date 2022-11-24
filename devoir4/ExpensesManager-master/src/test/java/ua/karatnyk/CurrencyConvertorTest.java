63fe984ece5a641042bad33ff7480b1da563cd57
manpingli <limanping@hotmail.fr>
2022-11-23 22:08:45 -0500

    add CurrencyConvertorTest.java

---
+++ devoir4/ExpensesManager-master/src/test/java/ua/karatnyk/CurrencyConvertorTest.java
@@ -0,0 +1,12 @@
+package ua.karatnyk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrencyConvertorTest extends Object {
+
+    @Test
+    public void convert() {
+    }
+}
