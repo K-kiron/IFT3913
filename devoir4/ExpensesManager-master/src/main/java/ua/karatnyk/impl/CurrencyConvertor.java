4f9faae6d281a348b06d9a7c36ea4e2144404d2c
manpingli <limanping@hotmail.fr>
2022-12-02 10:57:46 -0500

    add report

--- devoir4/ExpensesManager-master/src/main/java/ua/karatnyk/impl/CurrencyConvertor.java
+++ devoir4/ExpensesManager-master/src/main/java/ua/karatnyk/impl/CurrencyConvertor.java
@@ -4,13 +4,29 @@ import java.text.ParseException;
 
 public final class CurrencyConvertor {
 	
-	public static double convert(double amount, String from, String to, CurrencyConversion conversion) throws ParseException{
-		if(!conversion.getRates().containsKey(to)||!conversion.getRates().containsKey(from))
+//	public static double convert(double amount, String from, String to, CurrencyConversion conversion) throws ParseException{
+//		if(!conversion.getRates().containsKey(to)||!conversion.getRates().containsKey(from))
+//			throw new ParseException("Not correct format currency"
+//					+ "", 0);
+//		double curencyTo = conversion.getRates().get(to);
+//		double curencyFrom = conversion.getRates().get(from);
+//		return amount*(curencyTo/curencyFrom);
+//
+//	}
+public static double convert(double amount, String from, String to, CurrencyConversion conversion) throws ParseException{
+	if(!conversion.getRates().containsKey(to)){
+		throw new ParseException("Not correct format currency"
+				+ "", 0);
+	}else{
+		if(!conversion.getRates().containsKey(from)){
 			throw new ParseException("Not correct format currency"
 					+ "", 0);
-		double curencyTo = conversion.getRates().get(to);
-		double curencyFrom = conversion.getRates().get(from);
-		return amount*(curencyTo/curencyFrom);
-		
+		}else{
+			double curencyTo = conversion.getRates().get(to);
+			double curencyFrom = conversion.getRates().get(from);
+			return amount*(curencyTo/curencyFrom);
+
+		}
 	}
+}
 }
