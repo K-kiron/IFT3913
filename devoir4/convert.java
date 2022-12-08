3cd454d20255c46b66466a8fb51a8e24daa4fee5
K-kiron <70655683+K-kiron@users.noreply.github.com>
2022-12-08 15:58:58 -0500

    update

--- devoir4/convert.java
+++ devoir4/convert.java
@@ -1,9 +1,7 @@
-	public static double convert(double amount, String from, String to, CurrencyConversion conversion) throws ParseException{
-		if(!conversion.getRates().containsKey(to)||!conversion.getRates().containsKey(from))
-			throw new ParseException("Not correct format currency"
-					+ "", 0);
-		double curencyTo = conversion.getRates().get(to);
-		double curencyFrom = conversion.getRates().get(from);
-		return amount*(curencyTo/curencyFrom);
-		
-	}
+public static double convert(double amount, String from, String to, CurrencyConversion conversion) throws ParseException{
+	if(!conversion.getRates().containsKey(to)||!conversion.getRates().containsKey(from))
+		throw new ParseException("Not correct format currency"+ "", 0);
+	double curencyTo = conversion.getRates().get(to);
+	double curencyFrom = conversion.getRates().get(from);
+	return amount*(curencyTo/curencyFrom);
+}
